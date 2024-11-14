0dd2070af1d50748928a1ce5aad0c59ce861ab66
cku7808 <cku7808@naver.com>
2024-11-14 11:28:30 +0900

    feat: Add url patterns in fitfin project #8

--- fitfin/urls.py
+++ fitfin/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/v1/', include('finances.urls')),
+    path('accounts', include('dj_rest_auth.urls')),
+    path('accounts/signup/', include('dj_rest_auth.registration.urls')),
 ]
