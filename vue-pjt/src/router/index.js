2e831b83e27500c2920f412813da909349bb023a
cku7808 <cku7808@naver.com>
2024-11-19 11:17:43 +0900

    feat: Add menu navbar and link pages #17

--- vue-pjt/src/router/index.js
+++ vue-pjt/src/router/index.js
@@ -3,6 +3,12 @@ import SignUpView from '@/views/SignUpView.vue'
 import LogInView from '@/views/LogInView.vue'
 import MainView from '@/views/MainView.vue'
 import KakaoLogin from '@/components/KakaoLogin.vue'
+import CompareProductView from '@/views/CompareProductView.vue'
+import ExchangeRateView from '@/views/ExchangeRateView.vue'
+import BankPosView from '@/views/BankPosView.vue'
+import CommunityView from '@/views/CommunityView.vue'
+import MyPageView from '@/views/MyPageView.vue'
+import CartView from '@/views/CartView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +32,36 @@ const router = createRouter({
       path: '/kakaologin',
       name: 'KakaoLogin',
       component: KakaoLogin,
+    },
+    {
+      path: '/compareproduct',
+      name: 'CompareProductView',
+      component: CompareProductView,
+    },
+    {
+      path: '/exchangerate',
+      name: 'ExchangeRateView',
+      component: ExchangeRateView,
+    },
+    {
+      path: '/bankpos',
+      name: 'BankPosView',
+      component: BankPosView,
+    },
+    {
+      path: '/community',
+      name: 'CommunityView',
+      component: CommunityView,
+    },
+    {
+      path: '/mypage',
+      name: 'MyPageView',
+      component: MyPageView,
+    },
+    {
+      path: '/cart',
+      name: 'CartView',
+      component: CartView,
     },
   ],
 })
---
+++ vue-pjt/src/views/ExchangeRateView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        환율 계산 페이지
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
